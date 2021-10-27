dd86cfa395fb6849199f69259b62dd19a4c50d4a
acong <894960287@qq.com>
2021-10-27 18:08:04 +0800

    Upload the whole project

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="login-container">
+    <h1>登录页面</h1>
+    <div class="font-login">
+      <label for="username">登录名称：</label>
+      <input type="text" id="username" class="form-control" placeholder="请输入登录名称" v-model.trim="username">
+      <br>
+
+      <label for="passworde">登录密码：</label>
+      <input type="password" id="password" class="form-control" placeholder="请输入登录密码" v-model.trim="password">
+      <br>
+
+      <button type="button" class="btn" @click="reset">重置</button>
+      <button type="button" class="btn">登录</button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      reset() {
+        this.username = ''
+        this.password = ''
+      }
+    }
+  }
+
+</script>
+
+<style lang="less" scoped>
+  .login-container {
+    font-size: 18px;
+    /* text-align: center; */
+    width: 100%;
+    height: 100vh;
+    background: #bad2ec
+  }
+
+  .btn {
+    width: 50px;
+    height: 30px;
+    background-color: #40c1e9;
+  }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import Login from './components/Login.vue'
+
+Vue.use(ElementUI)
+
+Vue.component('Login', Login)
 
 Vue.config.productionTip = false
 
